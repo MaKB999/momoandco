a266d38934a6d1969b82835310ba5af5e624d391
MaKB999 <d.nevoltris@gmail.com>
2025-02-10 23:41:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 8080; // Use Render's dynamic port
+
 app.use(express.static(path.join(__dirname, 'cmc')));
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'cmc', 'index.html'));
 });
 
-app.listen(8080, () => {
-    console.log("Server successfully running on port 8080");
-  });
+app.listen(PORT, () => {
+    console.log(`Server successfully running on port ${PORT}`);
+});
